66d6e90138c040ff43f17f72860b6e12201e761d
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-03-31 17:58:44 +0800

    Update ActionArriveCanvasCC.ts

--- Game2D/assets/scripts/ActionArriveCanvasCC.ts
+++ Game2D/assets/scripts/ActionArriveCanvasCC.ts
@@ -1,10 +1,19 @@
-import { _decorator, Component, Node, director, Sprite, Scene, SceneAsset } from 'cc';
+import { _decorator, Component, Node, director, Sprite, Scene, SceneAsset, EventTouch, v2, v3, UITransform } from 'cc';
+import { STSteeredVehicle } from './STSteeredVehicle';
 import { STButtonNode, STSpriteNode } from './STUI';
 const { ccclass, property } = _decorator;
 
+const _temp_v2 = v2()
+const _temp_v3 = v3()
+
 @ccclass('ActionArriveCanvasCC')
 export class ActionArriveCanvasCC extends Component {
+
+
     private spriteBGNode: STSpriteNode = new STSpriteNode()
+    private targetNode: STSpriteNode = new STSpriteNode()
+    private bulletNode: STSpriteNode = new STSpriteNode()
+    private seeker: STSteeredVehicle
 
     __preload() {
         this.node.addChild(this.spriteBGNode)
@@ -23,6 +32,13 @@ export class ActionArriveCanvasCC extends Component {
         backBtnNode.st_top = 20
         backBtnNode.on(Node.EventType.TOUCH_END, this.onClickBack, this)
         this.node.addChild(backBtnNode)
+
+        this.node.addChild(this.targetNode)
+        this.targetNode.loadDir("actionArrive/target")
+
+        this.node.addChild(this.bulletNode)
+        this.bulletNode.loadDir("actionArrive/bullet")
+        this.seeker = this.bulletNode.addComponent(STSteeredVehicle).getComponent(STSteeredVehicle)
     }
 
     onLoad() {
@@ -31,6 +47,21 @@ export class ActionArriveCanvasCC extends Component {
 
     start() {
         console.debug("ActionArriveCanvasCC start")
+        this.node.on(Node.EventType.TOUCH_START, this.onTouchEvent, this)
+        this.node.on(Node.EventType.TOUCH_MOVE, this.onTouchEvent, this)
+        this.node.on(Node.EventType.TOUCH_END, this.onTouchEvent, this)
+    }
+    private onTouchEvent(evt: EventTouch) {
+        evt.getUILocation(_temp_v2)
+        _temp_v3.set(_temp_v2.x, _temp_v2.y, 0)
+        this.node.getComponent(UITransform)?.convertToNodeSpaceAR(_temp_v3, _temp_v3)
+        this.targetNode.setPosition(_temp_v3)
+    }
+
+    update() {
+        _temp_v2.set(this.targetNode.position.x, this.targetNode.position.y)
+        this.seeker.arrive(_temp_v2)
+        this.seeker.fixedUpdate()
     }
 
     onClickBack() {
