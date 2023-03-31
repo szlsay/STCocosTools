49685bb0f902b7696fb7f8879291538a6d88dd2d
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-03-31 15:32:43 +0800

    btn

--- Game2D/assets/scripts/HomeCanvasCC.ts
+++ Game2D/assets/scripts/HomeCanvasCC.ts
@@ -1,23 +1,44 @@
 import { _decorator, Component, Node, Sprite, resources, SpriteFrame, Color } from 'cc';
-import { STSpriteNode } from './STUI';
+import { STButtonNode, STLabelNode, STSpriteNode } from './STUI';
 const { ccclass, property } = _decorator;
 
 @ccclass('HomeCanvasCC')
 export class HomeCanvasCC extends Component {
 
-    private spriteNode: STSpriteNode = new STSpriteNode()
+    private spriteBGNode: STSpriteNode = new STSpriteNode()
+    private iconBGNode: STSpriteNode = new STSpriteNode()
+    private buttonNode: STButtonNode = new STButtonNode()
+    private btnLabelNode: STLabelNode = new STLabelNode()
     __preload() {
         console.debug("HomeCanvasCC __preload")
-        this.node.addChild(this.spriteNode)
-        this.spriteNode.sprite.sizeMode = Sprite.SizeMode.CUSTOM
-        this.spriteNode.st_left = 0
-        this.spriteNode.st_right = 0
-        this.spriteNode.st_top = 0
-        this.spriteNode.st_bottom = 0
-        this.spriteNode.st_colorHexString = "#CBFF8D"
+        this.node.addChild(this.spriteBGNode)
+        this.spriteBGNode.sprite.sizeMode = Sprite.SizeMode.CUSTOM
+        this.spriteBGNode.st_left = 0
+        this.spriteBGNode.st_right = 0
+        this.spriteBGNode.st_top = 0
+        this.spriteBGNode.st_bottom = 0
+        this.spriteBGNode.st_colorHexString = "#CBFF8D"
+        this.spriteBGNode.loadDir("home/default_sprite")
 
-        // #CBFF8D
-        this.spriteNode.loadDir("home/default_sprite")
+        this.node.addChild(this.iconBGNode)
+        this.iconBGNode.sprite.sizeMode = Sprite.SizeMode.CUSTOM
+        this.iconBGNode.st_left = 0
+        this.iconBGNode.st_right = 0
+        this.iconBGNode.st_top = 0
+        this.iconBGNode.st_bottom = 0
+        this.iconBGNode.loadDir("home/home_bg")
+
+        this.node.addChild(this.buttonNode)
+        this.buttonNode.sprite.sizeMode = Sprite.SizeMode.CUSTOM
+        this.buttonNode.st_width = 200
+        this.buttonNode.st_height = 80
+        this.buttonNode.loadDir("home/home_btn_bg")
+
+        this.buttonNode.addChild(this.btnLabelNode)
+        this.btnLabelNode.st_string = "案例1"
+        this.btnLabelNode.st_colorHexString = "#639F28"
+        this.btnLabelNode.st_fontSize = 30
+        this.btnLabelNode.setPosition(0, 4)
     }
 
     onLoad() {
