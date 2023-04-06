f7aa6d58d0a355f1e3ea411486bbec3aa56f8c98
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-04-06 09:21:47 +0800

    RollBallController

---
+++ Game2D/assets/scripts/RollBallController.ts
@@ -0,0 +1,5 @@
+import { _decorator } from "cc";
+import { STController } from "./STController";
+const { ccclass, property } = _decorator;
+@ccclass('RollBallController')
+export class RollBallController extends STController {}
