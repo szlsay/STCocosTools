33a19d9b04496c86a655527c04265b2db55c9648
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-04-06 11:02:46 +0800

    native

--- Game2D/assets/scripts/NativeController.ts
+++
@@ -1,5 +0,0 @@
-import { _decorator } from "cc";
-import { STController } from "./STController";
-const { ccclass, property } = _decorator;
-@ccclass('NativeController')
-export class NativeController extends STController {}
